24e7e5271d66ffb824cabda2ea2ad36e690a8eaa
Carnë Draug <carandraug+dev@gmail.com>
2013-01-13 16:27:18 +0000

    show: octave compatibility
      * octave's subplot works fine
      * fix possible matlab short circuit operator warning
      * use onCleanup to restore warnings in case of error before the end

--- Misc/show.m
+++ Misc/show.m
@@ -51,24 +51,24 @@
 % April     2007  Proper recording and restoring of MATLAB warning state.
 % September 2008  Octave compatible
 % May       2009  Reworked argument handling logic for extra flexibility
+% January   2013  More Octave compatibility and proper restoring of warning state (Carnë Draug)
 
 function h = show(im, param2, param3)
 
     Octave = exist('OCTAVE_VERSION', 'builtin') == 5; % Are we running under Octave
-    
-    if ~Octave
-	s = warning('query','all'); % Record existing warning state.
-	warning('off');             % Turn off warnings that might arise if image
-                                    % has to be rescaled to fit on screen
-    end
 
-    % Check case where im is an image filename rather than image data            
-    if ~isnumeric(im) & ~islogical(im) 
+    s = warning('query','all');                 % Record existing warning state.
+    warn_state = onCleanup (@() warning(s));    % Restore warnings at the end
+    warning('off');                             % Turn off warnings that might arise if image
+                                                % has to be rescaled to fit on screen
+
+    % Check case where im is an image filename rather than image data
+    if ~isnumeric(im) && ~islogical(im) 
         Title = im;            % Default title is file name
         im = imread(im);
     else
-        Title = inputname(1);  % Default title is variable name of image data        
-    end    
+        Title = inputname(1);  % Default title is variable name of image data
+    end
 
     figNo = -1;                % Default value indicating create new figure
     
@@ -83,56 +83,45 @@ function h = show(im, param2, param3)
             error('2nd argument must be a figure number or title');
         end
     elseif nargin == 3
-        figNo = param2;  
+        figNo = param2;
         Title = param3;
-    
+
         if ~strcmp(class(Title),'char')
             error('Title must be a string');
         end
         
-        if ~isnumeric(param2) || length(param2) ~= 1    
+        if ~isnumeric(param2) || length(param2) ~= 1
             error('Figure number must be an integer');
         end
-    end        
+    end
 
     if figNo > 0           % We have a valid figure number
         figure(figNo);     % Reuse or create a figure window with this number
-        if ~Octave			       
-            subplot('position',[0 0 1 1]); % Use the whole window
-        end
+        subplot('position',[0 0 1 1]); % Use the whole window
     elseif figNo == -1
         figNo = figure;        % Create new figure window
-        if ~Octave
-            subplot('position',[0 0 1 1]); % Use the whole window
-        end	    
+        subplot('position',[0 0 1 1]); % Use the whole window
     end
 
     if ndims(im) == 2          % Display as greyscale
-	imagesc(im);
-	colormap('gray');
+        imagesc(im);
+        colormap('gray');
     else
-	imshow(im);            % Display as RGB
+        imshow(im);            % Display as RGB
     end
-    
+
     if figNo == 0                 % Assume we are trying to do a subplot 
         figNo = gcf;              % Get the current figure number
-	axis('image'); axis('off');
-	title(Title);             % Use a title rather than rename the figure
+        axis('image'); axis('off');
+        title(Title);             % Use a title rather than rename the figure
     else
-	axis('image'); axis('off');
-	set(figNo,'name', ['  ' Title])
-	if ~Octave
-	    truesize(figNo);
-	end                                  
+        axis('image'); axis('off');
+        set(figNo,'name', ['  ' Title])
+        if ~Octave
+            truesize(figNo);
+        end
     end
-    
+
     if nargout == 1
        h = figNo;
     end
-
-    if ~Octave
-	warning(s);  % Restore warnings
-    end
-    
-
-
